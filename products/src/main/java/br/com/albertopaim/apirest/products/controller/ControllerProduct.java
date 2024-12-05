e46d9cf91147de1b42c7af8bd0a28865e6b5ef35
Alberto Paim <albertoopaim@gmail.com>
2024-12-05 09:51:57 -0300

    final

--- products/src/main/java/br/com/albertopaim/apirest/products/controller/ControllerProduct.java
+++ products/src/main/java/br/com/albertopaim/apirest/products/controller/ControllerProduct.java
@@ -4,6 +4,7 @@ import br.com.albertopaim.apirest.products.model.Produto;
 import br.com.albertopaim.apirest.products.repository.ProductRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -19,25 +20,29 @@ public class ControllerProduct {
     public Produto create(@RequestBody Produto produto) {
         var id = UUID.randomUUID();
         produto.setId(String.valueOf(id));
-
         productRepository.save(produto);
         return produto;
 
     }
 
     @GetMapping("/{id}")
-    public Produto getProduct(@PathVariable String id){
-    return productRepository.findById(id).orElse(null);
+    public Produto getProduct(@PathVariable String id) {
+        return productRepository.findById(id).orElse(null);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteProduct(@PathVariable String id){
-    productRepository.deleteById(id);
+    public void deleteProduct(@PathVariable String id) {
+        productRepository.deleteById(id);
     }
 
     @PutMapping("/{id}")
-    public void updateProduct(@PathVariable String id, @RequestBody Produto produto){
-    produto.setId(id);
-    productRepository.save(produto);
+    public void updateProduct(@PathVariable String id, @RequestBody Produto produto) {
+        produto.setId(id);
+        productRepository.save(produto);
+    }
+
+    @GetMapping
+    public List<Produto> getProducts(@RequestParam("name") String name) {
+        return productRepository.findByName(name);
     }
 }
--- products/src/main/java/br/com/albertopaim/apirest/products/repository/ProductRepository.java
+++ products/src/main/java/br/com/albertopaim/apirest/products/repository/ProductRepository.java
@@ -3,5 +3,8 @@ package br.com.albertopaim.apirest.products.repository;
 import br.com.albertopaim.apirest.products.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Produto, String> {
+    List<Produto> findByName(String name);
 }
